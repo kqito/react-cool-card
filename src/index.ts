9d18811fd4cc1c3f97d41fbe53b8ebf044889188
kqito <29191111+kqito@users.noreply.github.com>
2020-02-26 20:34:57 +0900

    Hotfix to export CoolCardEmoji

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { CoolCard } from "./components/CoolCard";
 export { CoolCardImage } from "./components/CoolCardImage";
+export { CoolCardEmoji } from "./components/CoolCardEmoji";
 export { CoolCardText } from "./components/CoolCardText";
